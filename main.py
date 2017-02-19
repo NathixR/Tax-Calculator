bd667d2ec48b9f66fd13d4b581c1c80bfa3ecfdf
Nathan Robshaw <nathan.robshaw@gmail.com>
2017-02-19 13:00:02 +0000

    Completes Tax Calculations

--- main.py
+++ main.py
@@ -4,35 +4,38 @@ print("")
 initial_salary_in_pence = int(input("What is your salary (in pence): "))
 
 # gross pay
-print("Gross Pay:")
-print(format_currency(initial_salary_in_pence))
-print("")
+print("\nGross Pay:\t\t\t", format_currency(initial_salary_in_pence))
 
 # calculate tax free allowance
-print("Tax Free Allowance:")
 result = tax_free_allowance(initial_salary_in_pence)
-print(format_currency(result))
-print("")
-
-# calculate taxable ammount
-print("Taxable Ammount:")
-result = taxable_ammount(initial_salary_in_pence)
-print(format_currency(result))
-print("")
+print("\nTax Free Allowance:\t\t", format_currency(result))
 
 # calculate national insurance
-print("National Insurance Total:")
 result = total_national_insurance(initial_salary_in_pence)
-print(format_currency(result))
-print("")
+print("\nNational Insurance Total:\t", format_currency(result))
 
-# tax at 20%
-# tax at 40%
-# tax at 45%
-# total tax
+# calculate taxable ammount
+result = taxable_ammount(initial_salary_in_pence)
+print("\nTaxable Ammount:\t\t", format_currency(result))
+
+# total tax at 20%
+result = tax_rate_at_20(taxable_ammount(initial_salary_in_pence))
+print("\nTotal Tax at 20%:\t\t", format_currency(result))
+
+# total tax at 40%
+result = tax_rate_at_40(taxable_ammount(initial_salary_in_pence))
+print("\nTotal Tax at 40%:\t\t", format_currency(result))
+
+# # total tax at 45%
+result = tax_rate_at_45(taxable_ammount(initial_salary_in_pence))
+print("\nTotal Tax at 45%:\t\t", format_currency(result))
+
+# total tax due
+result = total_tax(initial_salary_in_pence)
+print ("\nTotal Tax:\t\t\t", format_currency(result))
 
 # calculate net wage
-print("Net Wage:")
 result = net_wage(initial_salary_in_pence)
-print(format_currency(result))
-print("")
+print("\nNet Wage:\t\t\t", format_currency(result))
+
+print(input("\nPress the anykey to exit."))
--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,8 @@ NI_UPPER_THRESHOLD = 4300000
 NI_LOWER_RATE = 12
 NI_UPPER_RATE = 2
 TAX_FREE_ALLOWANCE = 1100000
+TAX_FREE_LOWER_THRESHOLD = 10000000
+TAX_FREE_UPPER_THRESHOLD = 12200000
 TAX_RATE_40_THRESHOLD = 3200000
 TAX_RATE_45_THRESHOLD = 15000000
 
@@ -16,7 +18,12 @@ def format_currency(priceInPence):
 
 # calculate tax free allowance
 def tax_free_allowance(full_salary):
-    if full_salary >= TAX_FREE_ALLOWANCE:
+    if full_salary > TAX_FREE_UPPER_THRESHOLD:
+        return 0
+    elif full_salary > TAX_FREE_LOWER_THRESHOLD:
+        offset = (full_salary - TAX_FREE_LOWER_THRESHOLD) / 2
+        return TAX_FREE_ALLOWANCE - offset
+    elif full_salary >= TAX_FREE_ALLOWANCE:
         return TAX_FREE_ALLOWANCE
     else:
         return full_salary
@@ -40,20 +47,58 @@ def lower_national_insurance(full_salary):
 def upper_national_insurance(full_salary):
     if full_salary <= NI_UPPER_THRESHOLD: # if below threshold, nothing to pay
         return 0
-    else: #
+    else:
         ratable_amount = full_salary - NI_UPPER_THRESHOLD
         return ratable_amount * (NI_UPPER_RATE / 100)
 
 # calculate total national insurance
 def total_national_insurance(full_salary):
-    total = lower_national_insurance(full_salary) + upper_national_insurance(full_salary)
+    lower = lower_national_insurance(full_salary)
+    upper = upper_national_insurance(full_salary)
+    total = lower + upper
     return total
 
-# calculate tax rate
-def tax_rate(full_salary):
-    if full_salary < TAX_RATE_40_THRESHOLD:
-        return
+# total tax
+def total_tax(full_salary):
+    total_taxable = taxable_ammount(full_salary)
+    taxat20 = tax_rate_at_20(total_taxable)
+    taxat40 = tax_rate_at_40(total_taxable)
+    taxat45 = tax_rate_at_45(total_taxable)
+    total = taxat20 + taxat40 + taxat45
+    return total
+
+# tax at 20%
+def tax_rate_at_20(total_taxable):
+    if total_taxable <= TAX_FREE_ALLOWANCE:
+        return 0
+    elif total_taxable <= TAX_RATE_40_THRESHOLD:
+        return total_taxable * (TAX_RATE_20 / 100)
+    else:
+        taxable = TAX_RATE_40_THRESHOLD
+        return taxable * (TAX_RATE_20 / 100)
+
+# tax at 40%
+def tax_rate_at_40(total_taxable):
+    if total_taxable <= TAX_RATE_40_THRESHOLD:
+        return 0
+    elif total_taxable <= TAX_RATE_45_THRESHOLD:
+        taxable = total_taxable - TAX_RATE_40_THRESHOLD
+        return taxable * (TAX_RATE_40 / 100)
+    else:
+        taxable = TAX_RATE_45_THRESHOLD - TAX_RATE_40_THRESHOLD
+        return taxable * (TAX_RATE_40 / 100)
+
+# tax at 45%
+def tax_rate_at_45(total_taxable):
+    if total_taxable <= TAX_RATE_45_THRESHOLD:
+        return 0
+    else:
+        taxable = total_taxable - TAX_RATE_45_THRESHOLD
+        return taxable * (TAX_RATE_45 / 100)
 
 # calculate net wage
 def net_wage(full_salary):
-    return full_salary
+    ni = total_national_insurance(full_salary)
+    tax = total_tax(full_salary)
+    total = full_salary - ni - tax
+    return total

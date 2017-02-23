24143f142ad57f36ba7c7e4664b2d4f5c928b220
Nathan Robshaw <nathan.robshaw@gmail.com>
2017-02-23 22:54:07 +0000

    Change Formatting and Add Comments

--- main.py
+++ main.py
@@ -1,41 +1,121 @@
 from src import *
-print("")
 
-initial_salary_in_pence = int(input("What is your salary (in pence): "))
+gross_salary = None
+
+while not gross_salary:
+    try:
+        gross_salary = float(input("\nWhat is your salary: "))
+    except ValueError or NameError:
+        print("You need to put in a number!")
 
 # gross pay
-print("\nGross Pay:\t\t\t", format_currency(initial_salary_in_pence))
+result = gross_salary
+print_row(
+    "Gross Pay",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # calculate tax free allowance
-result = tax_free_allowance(initial_salary_in_pence)
-print("\nTax Free Allowance:\t\t", format_currency(result))
+result = tax_free_allowance(gross_salary)
+print_row(
+    "Tax Free Allowance",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # calculate national insurance
-result = total_national_insurance(initial_salary_in_pence)
-print("\nNational Insurance Total:\t", format_currency(result))
+result = total_national_insurance(gross_salary)
+print_row(
+    "National Insurance",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # calculate taxable ammount
-result = taxable_ammount(initial_salary_in_pence)
-print("\nTaxable Ammount:\t\t", format_currency(result))
+result = taxable_ammount(gross_salary)
+print_row(
+    "Taxable Ammount",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # total tax at 20%
-result = tax_rate_at_20(taxable_ammount(initial_salary_in_pence))
-print("\nTotal Tax at 20%:\t\t", format_currency(result))
+result = tax_rate_at_20(taxable_ammount(gross_salary))
+print_row(
+    "Total Tax at 20%",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # total tax at 40%
-result = tax_rate_at_40(taxable_ammount(initial_salary_in_pence))
-print("\nTotal Tax at 40%:\t\t", format_currency(result))
+result = tax_rate_at_40(taxable_ammount(gross_salary))
+print_row(
+    "Total Tax at 40%",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # # total tax at 45%
-result = tax_rate_at_45(taxable_ammount(initial_salary_in_pence))
-print("\nTotal Tax at 45%:\t\t", format_currency(result))
+result = tax_rate_at_45(taxable_ammount(gross_salary))
+print_row(
+    "Total Tax at 45%",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # total tax due
-result = total_tax(initial_salary_in_pence)
-print ("\nTotal Tax:\t\t\t", format_currency(result))
+result = total_tax(gross_salary)
+print_row(
+    "Total Tax",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
+
+# total deductions
+result = total_deductions(gross_salary)
+print_row(
+    "Total Deductions",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 # calculate net wage
-result = net_wage(initial_salary_in_pence)
-print("\nNet Wage:\t\t\t", format_currency(result))
+result = net_wage(gross_salary)
+print_row(
+    "Net Wage",
+    format_currency(result),
+    format_currency(monthly_ammount(result)),
+    format_currency(weekly_ammount(result)),
+    format_currency(daily_ammount(result)),
+    total_percentage(result, gross_salary)
+)
 
 print(input("\nPress the anykey to exit."))
--- src/__init__.py
+++ src/__init__.py
@@ -2,24 +2,78 @@
 TAX_RATE_20 = 20
 TAX_RATE_40 = 40
 TAX_RATE_45 = 45
-NI_LOWER_THRESHOLD = 806000
-NI_UPPER_THRESHOLD = 4300000
+NI_LOWER_THRESHOLD = 8060.00
+NI_UPPER_THRESHOLD = 43000.00
 NI_LOWER_RATE = 12
 NI_UPPER_RATE = 2
-TAX_FREE_ALLOWANCE = 1100000
-TAX_FREE_LOWER_THRESHOLD = 10000000
-TAX_FREE_UPPER_THRESHOLD = 12200000
-TAX_RATE_40_THRESHOLD = 3200000
-TAX_RATE_45_THRESHOLD = 15000000
+TAX_FREE_ALLOWANCE = 11000.00
+TAX_FREE_LOWER_THRESHOLD = 100000.00
+TAX_FREE_UPPER_THRESHOLD = 122000.00
+TAX_RATE_40_THRESHOLD = 32000.00
+TAX_RATE_45_THRESHOLD = 150000.00
 
+# formats the result in pounds
 def format_currency(priceInPence):
-    price = priceInPence / 100
-    return "£{0:.2f}".format(price)
+    return "£{0:.2f}".format(priceInPence)
+
+# formats the printed results
+def print_row(title, value, monthly, weekly, daily, percentage):
+    print("\n%s:\n%-12s%-12s%-12s%-12s%i%%" % \
+    (title, value, monthly, weekly, daily, int(percentage)))
+
+# calculates and displays the percentage of overall wages
+def total_percentage(value, initial_salary):
+    return (value / initial_salary) * 100
+
+# calculates monthly ammount, so result devided by 12
+def monthly_ammount(result):
+    return result / 12
+
+# calculate weekly ammount, so result divided by 52
+def weekly_ammount(result):
+    return result / 52
+
+# calculates the daily ammount, so divides the result by 260
+def daily_ammount(result):
+    return result / 260
+
+# calculate lower national insurance
+def lower_national_insurance(full_salary):
+    # if below lower threshold, nothing to pay
+    if full_salary <= NI_LOWER_THRESHOLD:
+        return 0
+    # if below upper threshold, pay the lower rate on salary minus lower limit
+    elif full_salary <= NI_UPPER_THRESHOLD:
+        ratable_amount = full_salary - NI_LOWER_THRESHOLD
+        return ratable_amount * (NI_LOWER_RATE / 100)
+    # else pay the lower rate on salary between lower and upper limit
+    else:
+        ratable_amount = NI_UPPER_THRESHOLD - NI_LOWER_THRESHOLD
+        return ratable_amount * (NI_LOWER_RATE / 100)
+
+# calculate upper national insurance
+def upper_national_insurance(full_salary):
+    # if below higher threshold, nothing to pay
+    if full_salary <= NI_UPPER_THRESHOLD:
+        return 0
+    # if above the threshold, pay rate on salary above the upper threshold
+    else:
+        ratable_amount = full_salary - NI_UPPER_THRESHOLD
+        return ratable_amount * (NI_UPPER_RATE / 100)
+
+# calculate total national insurance
+def total_national_insurance(full_salary):
+    lower = lower_national_insurance(full_salary)
+    upper = upper_national_insurance(full_salary)
+    total = lower + upper
+    return total
 
 # calculate tax free allowance
 def tax_free_allowance(full_salary):
     if full_salary > TAX_FREE_UPPER_THRESHOLD:
         return 0
+    # calculates the ammount to pay if between the £100,000 and £122,000 bracket
+    # for every £1 above 100,000, £2 is deducted from allowance
     elif full_salary > TAX_FREE_LOWER_THRESHOLD:
         offset = (full_salary - TAX_FREE_LOWER_THRESHOLD) / 2
         return TAX_FREE_ALLOWANCE - offset
@@ -32,43 +86,9 @@ def tax_free_allowance(full_salary):
 def taxable_ammount(full_salary):
     return full_salary - tax_free_allowance(full_salary)
 
-# calculate lower national insurance
-def lower_national_insurance(full_salary):
-    if full_salary <= NI_LOWER_THRESHOLD: # if below threshold, nothing to pay
-        return 0
-    elif full_salary <= NI_UPPER_THRESHOLD: # if below upper limit, pay rate on salary minus lower limit
-        ratable_amount = full_salary - NI_LOWER_THRESHOLD
-        return ratable_amount * (NI_LOWER_RATE / 100)
-    else: # if greater than upper limit, pay rate on salary below lower and upper limit
-        ratable_amount = NI_UPPER_THRESHOLD - NI_LOWER_THRESHOLD
-        return ratable_amount * (NI_LOWER_RATE / 100)
-
-# calculate upper national insurance
-def upper_national_insurance(full_salary):
-    if full_salary <= NI_UPPER_THRESHOLD: # if below threshold, nothing to pay
-        return 0
-    else:
-        ratable_amount = full_salary - NI_UPPER_THRESHOLD
-        return ratable_amount * (NI_UPPER_RATE / 100)
-
-# calculate total national insurance
-def total_national_insurance(full_salary):
-    lower = lower_national_insurance(full_salary)
-    upper = upper_national_insurance(full_salary)
-    total = lower + upper
-    return total
-
-# total tax
-def total_tax(full_salary):
-    total_taxable = taxable_ammount(full_salary)
-    taxat20 = tax_rate_at_20(total_taxable)
-    taxat40 = tax_rate_at_40(total_taxable)
-    taxat45 = tax_rate_at_45(total_taxable)
-    total = taxat20 + taxat40 + taxat45
-    return total
-
-# tax at 20%
+# tax at 20% rate
 def tax_rate_at_20(total_taxable):
+    #
     if total_taxable <= TAX_FREE_ALLOWANCE:
         return 0
     elif total_taxable <= TAX_RATE_40_THRESHOLD:
@@ -95,6 +115,19 @@ def tax_rate_at_45(total_taxable):
     else:
         taxable = total_taxable - TAX_RATE_45_THRESHOLD
         return taxable * (TAX_RATE_45 / 100)
+
+# calculate total tax
+def total_tax(full_salary):
+    total_taxable = taxable_ammount(full_salary)
+    taxat20 = tax_rate_at_20(total_taxable)
+    taxat40 = tax_rate_at_40(total_taxable)
+    taxat45 = tax_rate_at_45(total_taxable)
+    total = taxat20 + taxat40 + taxat45
+    return total
+
+# calculate total deductions
+def total_deductions(full_salary):
+    return full_salary - net_wage(full_salary)
 
 # calculate net wage
 def net_wage(full_salary):
